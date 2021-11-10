6dce57793923d6fcd5abac4c4328be99d2241f07
jtracos <slirahux@protonmail.ch>
2021-11-10 16:24:50 -0600

    Version 0.1

--- scrapper.py
+++ scrapper.py
@@ -2,23 +2,53 @@ import requests
 from lxml import html
 import autopep8
 import xpath
+from datetime import datetime
+import os
 
-def parse():
+def parse_note(url, date):
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            if not os.path.isdir(date):
+                os.mkdir(date)
+
+            text = response.content.decode("utf-8")
+            parsed_note = html.fromstring(text)
+            body = parsed_note.xpath(xpath.CORPUS)
+            title = parsed_note.xpath(xpath.TITLE_CORPUS)[0].replace("¿","").replace("?","").\
+                replace("¡","").replace("!","")
+            with open(f"./{date}/{title}.txt", mode="w") as f:
+                f.write(title.upper())
+                f.write("\n\n")
+                for line in body:
+                    f.write(line)
+                    f.write("\n")
+
+        else:
+            raise ValueError(f"Codigo de error: {response.status_code}")
+    except ValueError as ve:
+        print(ve)
+
+
+def parse_home():
     try:
         response = requests.get(xpath.TH)
         if response.status_code == 200:
             home = response.content.decode("utf-8")
             parsed = html.fromstring(home)
             links = parsed.xpath(xpath.LINKS)
+            DATE_STR = datetime.today().strftime("%d-%m-%Y")
             for link in links:
-                print(links)
+                parse_note(link, DATE_STR)
         else:
             raise ValueError(f"Error: {response.status_code}")
     except ValueError as ve:
         print(ve)
 
+
 def run():
-    parse()
+    parse_home()
+
 
 if __name__ == "__main__":
 
---
+++ xpath.py
@@ -0,0 +1,13 @@
+#smn
+imgFc24h = "//div[@class=\"col-lg-6 col-md-6\"]/div/a/img/@src"
+smn = "https://smn.conagua.gob.mx/es/pronosticos/pronosticossubmenu/pronostico-meteorologico-especial-cuencas-96h"
+#TH
+
+TH = 'https://www.tabascohoy.com'
+
+LINKS = '//div/div/a[@rel=\"bookmark\"]/div/div/h2/../../.././@href'
+TITLE = '//div/div/a[@rel="bookmark"]/div/div/h2/text()'
+
+CORPUS = '//div[@class="theiaPostSlider_preloadedSlide"]/p/text()'
+TITLE_CORPUS = '//h1[@class="mvp-post-title left entry-title"]/text()'
+DATE = '//time[@class="post-date updated"]/@datetime'
