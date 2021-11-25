29657020ab6d94ad1d88547f31f579b515620919
Josue <slirahux@protonmail.ch>
2021-11-24 22:12:32 -0600

    changed xpath, presente

--- xpath.py
+++ xpath.py
@@ -3,7 +3,16 @@ imgFc24h = "//div[@class=\"col-lg-6 col-md-6\"]/div/a/img/@src"
 smn = "https://smn.conagua.gob.mx/es/pronosticos/pronosticossubmenu/pronostico-meteorologico-especial-cuencas-96h"
 #TH
 
-TH = 'https://www.tabascohoy.com'
+#TH = 'https://www.tabascohoy.com'
 
-LINKS = '//div/div/a[@rel=\"bookmark\"]/div/div/h2/../../.././@href'
-TITLE = '//div/div/a[@rel="bookmark"]/div/div/h2/text()'
+#LINKS = '//div/div/a[@rel=\"bookmark\"]/div/div/h2/../../.././@href'
+#TITLE = '//div/div/a[@rel="bookmark"]/div/div/h2/text()'
+
+
+DP = "https://www.diariopresente.mx/"
+DP_LINKS = "//article/div[@class = 'detail']/div[@class='info']/span/i[@class='fa fa-calendar-o']/../../..//div[contains(@class,'caption')]/a/@href"
+DP_DATE = "//article/div[@class = 'detail']/div/span/i[@class='fa fa-calendar-o']/../text()"
+DP_HEADER = "//article/div[@class = 'detail']/div[@class='info']/span/i[@class='fa fa-calendar-o']/../../..//div[contains(@class,'caption')]/a/text()""
+
+DL_AR_TITLE = "//div/div[@class='caption']/text()"
+AR_RESUMEE = "//div[@class='detail']/div[contains(@style,'color')]/text()"
